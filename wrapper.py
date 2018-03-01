750fab28f069eaa978690f8bd4ab963ae079a686
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2018-03-01 21:00:31 +0100

    import 'hello' rather than using hy.importer

--- wrapper.py
+++ wrapper.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 
-import os
 import hy
-hy.importer.import_file_to_module(
-    "__main__",
-    os.path.join(os.path.dirname(os.path.realpath(__file__)), "hello.hy"))
+import hello
 
+
+if "__main__" == __name__:
+    print("hell")
+    hello.simple_conversation()
